2a4b744c403f7f466a86e615ef0024a5f5bb96a8
preetha912001-oss <preetha912001@gmail.com>
2026-09-02 11:54:42 +0530

    Add files via upload
    Day2 Assignment

---
+++ Day2/dataTypes.js
@@ -0,0 +1,33 @@
+// Day2 - Primitive Data Types
+
+//Number
+var mobileNumber = 25;
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+//String
+
+var firstName = 'Preetha';
+console.log(firstName);
+console.log(typeof firstName);
+
+//Boolean 
+
+var isAutomation = true;
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+//Null
+
+var companyName = null;
+console.log(companyName);
+console.log(typeof companyName);
+
+//Undefined
+
+var hasPlaywright;
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+
+
+
