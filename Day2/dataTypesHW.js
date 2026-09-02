df31037e773235994173f920353d8d7a0fbd521d
preetha912001-oss <preetha912001@gmail.com>
2026-09-02 12:29:30 +0530

    Add files via upload
    Day2HomeAssignment

---
+++ Day2/dataTypesHW.js
@@ -0,0 +1,35 @@
+
+// Day2 - Primitive Data Types Home Assignment
+
+// String
+let userName = "Preetha";
+console.log(userName);
+console.log(typeof userName);
+
+//String
+let organization = "RMESI";
+console.log(userName);
+console.log(typeof userName);
+
+//Number
+let contactNumber = 6755635868;
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+//Boolean
+let knowsAutomation = "true";
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+//Undefined
+let usesPlaywright;
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
+//Null
+let email = null;
+console.log(email);
+console.log(typeof email);
+
+
+
