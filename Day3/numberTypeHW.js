9ca8bd3f4e60032812395ec92016cb19319e6bd8
preetha912001-oss <preetha912001@gmail.com>
2026-09-03 07:19:22 +0530

    Add files via upload
    Day3 Home Assignments

---
+++ Day3/numberTypeHW.js
@@ -0,0 +1,13 @@
+
+// Checking whether the Number is positive,negative or zero
+
+let numberValue = 0
+function numberType(value){
+    if(value > 0)
+        console.log("The Entered number is Positive")
+    else if(value < 0)
+        console.log("The Entered number is Negative")
+    else
+        console.log("The Entered number is Neutral")
+}
+numberType(numberValue)
---
+++ Day3/odd-EvenHW.js
@@ -0,0 +1,11 @@
+
+// Checking, the userInput is Odd or Even
+
+var userInput = 12
+function isOddOrEven(value){  //Passing parameter as "value"
+   if(value%2 === 0)          
+    console.log('Even')
+   else
+    console.log('Odd')
+}
+isOddOrEven(userInput)   //Passing userInput as arguments
